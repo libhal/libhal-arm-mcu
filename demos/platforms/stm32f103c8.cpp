611741e94d4d46ad37b6f7a68336d739189cd61b
Khalil Estell <kammce0@gmail.com>
2024-08-02 17:31:39 -0700

    :recycle: Use libhal-arm-mcu directory for include

--- demos/platforms/stm32f103c8.cpp
+++ demos/platforms/stm32f103c8.cpp
@@ -12,17 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <libhal-armcortex/dwt_counter.hpp>
-#include <libhal-armcortex/startup.hpp>
-#include <libhal-armcortex/system_control.hpp>
+#include <libhal-arm-mcu/dwt_counter.hpp>
+#include <libhal-arm-mcu/startup.hpp>
+#include <libhal-arm-mcu/stm32f1/can.hpp>
+#include <libhal-arm-mcu/stm32f1/clock.hpp>
+#include <libhal-arm-mcu/stm32f1/constants.hpp>
+#include <libhal-arm-mcu/stm32f1/input_pin.hpp>
+#include <libhal-arm-mcu/stm32f1/output_pin.hpp>
+#include <libhal-arm-mcu/stm32f1/uart.hpp>
+#include <libhal-arm-mcu/system_control.hpp>
 #include <libhal-soft/bit_bang_i2c.hpp>
 #include <libhal-soft/inert_drivers/inert_adc.hpp>
-#include <libhal-stm32f1/can.hpp>
-#include <libhal-stm32f1/clock.hpp>
-#include <libhal-stm32f1/constants.hpp>
-#include <libhal-stm32f1/input_pin.hpp>
-#include <libhal-stm32f1/output_pin.hpp>
-#include <libhal-stm32f1/uart.hpp>
 
 #include <resource_list.hpp>
 

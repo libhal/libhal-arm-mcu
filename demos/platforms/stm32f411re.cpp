003ac77579f231fcc978e79d1de32bfa005b190e
Khalil Estell <khalil.estell@sjsu.edu>
2024-11-30 06:26:52 -0800

    :rotating_light: Fix lint (#70)

--- demos/platforms/stm32f411re.cpp
+++ demos/platforms/stm32f411re.cpp
@@ -15,13 +15,13 @@
 
 #include <libhal-arm-mcu/dwt_counter.hpp>
 #include <libhal-arm-mcu/startup.hpp>
-#include <libhal-arm-mcu/stm32f411/pin.hpp>
-#include <libhal-arm-mcu/system_control.hpp>
 #include <libhal-arm-mcu/stm32f411/clock.hpp>
 #include <libhal-arm-mcu/stm32f411/constants.hpp>
 #include <libhal-arm-mcu/stm32f411/input_pin.hpp>
 #include <libhal-arm-mcu/stm32f411/output_pin.hpp>
+#include <libhal-arm-mcu/stm32f411/pin.hpp>
 #include <libhal-arm-mcu/stm32f411/spi.hpp>
+#include <libhal-arm-mcu/system_control.hpp>
 #include <libhal/initializers.hpp>
 #include <libhal/output_pin.hpp>
 #include <libhal/units.hpp>
@@ -33,8 +33,9 @@ void initialize_platform(resource_list& p_resources)
   using namespace hal::literals;
   hal::stm32f411::maximum_speed_using_internal_oscillator();
 
-  auto cpu_frequency = hal::stm32f411::frequency(hal::stm32f411::peripheral::cpu);
-  static hal::cortex_m::dwt_counter steady_clock(cpu_frequency/8);
+  auto cpu_frequency =
+    hal::stm32f411::frequency(hal::stm32f411::peripheral::cpu);
+  static hal::cortex_m::dwt_counter steady_clock(cpu_frequency / 8);
 
   static hal::stm32f411::input_pin button(
     hal::stm32f411::peripheral::gpio_c,
@@ -43,8 +44,9 @@ void initialize_platform(resource_list& p_resources)
   static hal::stm32f411::output_pin led(hal::stm32f411::peripheral::gpio_a, 5);
 
   static hal::stm32f411::spi spi(hal::runtime{}, 2, {});
-  hal::stm32f411::output_pin chip_select(hal::stm32f411::peripheral::gpio_b, 13);
-  
+  hal::stm32f411::output_pin chip_select(hal::stm32f411::peripheral::gpio_b,
+                                         13);
+
   p_resources.reset = []() { hal::cortex_m::reset(); };
   p_resources.status_led = &led;
   p_resources.clock = &steady_clock;

2b4aec8ab55acb6c3d2728f2c7f93737b84ff249
Khalil Estell <kammce0@gmail.com>
2025-05-27 15:09:38 -0700

    :pushpin: Use range for python_requires

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Copyright 2024 Khalil Estell
+# Copyright 2024 - 2025 Khalil Estell and the libhal contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -34,7 +34,7 @@ class libhal_arm_mcu_conan(ConanFile):
               "stm32f1", "stm32f103")
     settings = "compiler", "build_type", "os", "arch"
 
-    python_requires = "libhal-bootstrap/[^4.2.1]"
+    python_requires = "libhal-bootstrap/[>=4.3.0 <5]"
     python_requires_extend = "libhal-bootstrap.library"
 
     options = {

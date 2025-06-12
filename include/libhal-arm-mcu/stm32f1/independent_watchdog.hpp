6508b9e68c8c200e6d3bec54ff8e576714f8fbe2
MichaelYKersey <144640226+MichaelYKersey@users.noreply.github.com>
2025-06-12 15:37:47 -0700

    frommating

--- include/libhal-arm-mcu/stm32f1/independent_watchdog.hpp
+++ include/libhal-arm-mcu/stm32f1/independent_watchdog.hpp
@@ -9,19 +9,20 @@ class independent_watchdog : public hal::watchdog
 {
 public:
   void start() override;
-
   void reset() override;
   /**
    * @brief configures the watchdog countdown frequency and counter to specified
    * time
    *
+   *
+   * @param p_wait_time coundown time till the watchdog resets
+   *
    * the actual wait time may be 66%-133% of the specified time due to clock
    * variance (sec. 7.2.5, pg. 96)
    */
-  void set_countdown_time(hal::time_duration wait_time) override;
+  void set_countdown_time(hal::time_duration p_wait_time) override;
 
   bool check_flag() override;
-
   void clear_flag() override;
 };
 

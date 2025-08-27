6bf217e62ac9b5b61c27f0f2fb0da297f8108c53
Khalil Estell <khalil.estell@sjsu.edu>
2025-08-26 19:23:19 -0700

    :sparkles: Add usb_raw demo to demos (#159)
    usb_raw performs enumeration directly in the file vs using an enumerator

--- demos/resource_list.hpp
+++ demos/resource_list.hpp
@@ -30,6 +30,7 @@
 #include <libhal/steady_clock.hpp>
 #include <libhal/stream_dac.hpp>
 #include <libhal/timer.hpp>
+#include <libhal/usb.hpp>
 #include <libhal/zero_copy_serial.hpp>
 
 namespace custom {
@@ -91,6 +92,23 @@ hal::v5::strong_ptr<hal::output_pin> spi_chip_select();
 hal::v5::strong_ptr<hal::stream_dac_u8> stream_dac();
 hal::v5::strong_ptr<hal::dac> dac();
 hal::v5::strong_ptr<custom::watchdog> watchdog();
+hal::v5::strong_ptr<hal::v5::usb_control_endpoint> usb_control_endpoint();
+
+hal::v5::strong_ptr<hal::v5::usb_interrupt_in_endpoint>
+usb_interrupt_in_endpoint1();
+hal::v5::strong_ptr<hal::v5::usb_interrupt_out_endpoint>
+usb_interrupt_out_endpoint1();
+
+hal::v5::strong_ptr<hal::v5::usb_interrupt_in_endpoint>
+usb_interrupt_in_endpoint2();
+hal::v5::strong_ptr<hal::v5::usb_interrupt_out_endpoint>
+usb_interrupt_out_endpoint2();
+
+hal::v5::strong_ptr<hal::v5::usb_bulk_in_endpoint> usb_bulk_in_endpoint1();
+hal::v5::strong_ptr<hal::v5::usb_bulk_out_endpoint> usb_bulk_out_endpoint1();
+
+hal::v5::strong_ptr<hal::v5::usb_bulk_in_endpoint> usb_bulk_in_endpoint2();
+hal::v5::strong_ptr<hal::v5::usb_bulk_out_endpoint> usb_bulk_out_endpoint2();
 
 inline void reset()
 {

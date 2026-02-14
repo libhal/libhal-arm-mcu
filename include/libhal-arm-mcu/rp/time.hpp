5000a35a8f9ef7fdc889c191dea5b26f2102b31d
Shin Umeda <umeda.shin@gmail.com>
2026-02-14 10:02:11 -0800

    add function to get core clock

--- include/libhal-arm-mcu/rp/time.hpp
+++ include/libhal-arm-mcu/rp/time.hpp
@@ -13,6 +13,9 @@ struct clock final : public hal::steady_clock
   u64 driver_uptime() override;
 };
 
+// returns configured clock speed for use with dwt_counter
+hertz core_clock();
+
 using microseconds = std::chrono::duration<u32, std::micro>;
 
 }  // namespace hal::rp::inline v4

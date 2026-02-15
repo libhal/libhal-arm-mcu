337d2358d9beeb8774c352b624894c4c33479418
Khalil Estell <khalil.estell@sjsu.edu>
2026-02-14 17:46:27 -0800

    :ambulance: (patch) Fix picolibc -lhosted build break (#191)

--- test_package/main.cpp
+++ test_package/main.cpp
@@ -12,9 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <array>
 #include <exception>
-#include <span>
 
 #include <libhal-arm-mcu/dwt_counter.hpp>
 

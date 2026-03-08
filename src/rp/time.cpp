ce5f6e81f52cfb0d0ec132eca3ca0fc7049176ce
Shin Umeda <umeda.shin@gmail.com>
2026-03-08 10:54:31 -0700

    Expose sleep interface

--- src/rp/time.cpp
+++ src/rp/time.cpp
@@ -3,6 +3,7 @@
 
 #include <hardware/platform_defs.h>
 #include <hardware/timer.h>
+#include <pico/time.h>
 
 namespace hal::rp::inline v4 {
 
@@ -19,6 +20,11 @@ u64 clock::driver_uptime()
 hertz core_clock()
 {
   return SYS_CLK_HZ;
+}
+
+void sleep(std::chrono::duration<u64, std::micro> time) noexcept
+{
+  sleep_us(time.count());
 }
 
 }  // namespace hal::rp::inline v4

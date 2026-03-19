8c7a32e7d2d2326d9229bd53f5a52e33745d7c39
Shin Umeda <umeda.shin@gmail.com>
2026-03-19 14:10:01 -0700

    disable waiting to allow for faster polling

--- src/rp/serial.cpp
+++ src/rp/serial.cpp
@@ -5,6 +5,7 @@
 #include <hardware/uart.h>
 #include <libhal/error.hpp>
 #include <libhal/units.hpp>
+#include <pico/error.h>
 #include <pico/stdio.h>
 #include <pico/time.h>
 
@@ -48,12 +49,9 @@ serial::write_t stdio_serial::driver_write(std::span<byte const> in)
 
 serial::read_t stdio_serial::driver_read(std::span<byte> output)
 {
-  // time in microseconds
-  auto now = get_absolute_time();
-  // 500 microseconds is 0.5 ms, which is probably reasonable
   int len = stdio_get_until(reinterpret_cast<char*>(output.data()),
                             static_cast<int>(output.size_bytes()),
-                            now + 500);
+                            0);
   return read_t{ .data = output.subspan(0, len),
                  .available = 0,
                  .capacity = 1 };
